99869d176ab37cb2b9da39bdfc01ce9a85f6cbe5
bizonpatrycja <36458654+bizonpatrycja@users.noreply.github.com>
2021-04-16 10:22:06 +0200

    Create Build a Car.java
    Ever wondered how it is to build cars? Today you can have a taste of it and build your own, very simplified version of car using your programming skills. We are going to build vehicles like these below: ____ __________ ____________ | []\ | []\ |[] []
    -o--o-' -o-o------o-' -o-o------o-o-'
    
    (Hope that those resemble cars|buses|trucks).
    Short introduction
    
    A single vehicle is built with 3 layers:
    
    first: ' ____' second: | []\ third: -o--o-'
    
        first layer (top) consists of one space character ' ' and underscores '_';
    
        second layer consists of a vertical line (rear) |, empty spaces (filling) ' ', square brackets (doors) [] and a backslash (front) \ ;
    
        third layer consists of dashes (frame) '-', lowercase O letters (axles|wheels) 'o' and one single quote (headlight) "'"
    
    Problem specification
    
    Your aim is to implement 'Car' class/constructor. The instance of 'Car' must contain 2 properties/members body and chassis which themselves are objects.
    
    Each instance object (body and chassis) must contain one property called component.
    
    Let's assume that car refers to an instance of Car, then tests can refer to component properties by: car.chassis.component and car.body.component.
    
    The component properties are of String type, and contain layers of the car. body.component constitutes of first two layers, whereas chassis.componentcontains the last, third layer.
    
    In Java, all these properties/objects have to be public.
    
    The Car constructor takes two integer arguments:
    
        length specifies the length of the car (in number of characters). Must be at least 7 characters wide (as the first car above), if not, an Exception | Error must be thrown;
        doors specifies the number of doors (square brackets) in the second layer. Must be at least 1, if not, an exception must be thrown. Another exception must be thrown when the doors parameter is too big for the current car (when we cannot fit that number of doors in the given length).
    
    The specified error cases are the only 3 expected (in Java: throw an unchecked exception), therefore the tests will use only positive integers as parameters to Car()
    Design guidelines
    
        _____________          ______          __________
       |[][]   [][][]\        |[][][]\        |        []\
       -o-o-o-----o-o-'       -o----o-'       -o-o------o-'
     
    
    As we can see, our Car class is quite flexible in terms of number of axles and doors, and here the following applies.
    
        As the length parameter increases, we want our cars|trucks to keep balance, hence the number of axles has to increase. The third axle has to be added at the length = 12 and subsequent axles have to be added every time the given length is wider by 2 (next axle at length = 14 and so on). As the parameter is big enough we add axles once to the rear (left), once to the front (right) part of the car - starting from the rear.
        The situation works similarly with the doors parameter. We try to fit the given number of doors in the given length. The first door is added to the front, the next to the rear, the next again to the front.
    
    One final guide:
    
        first layer's length = length - 2
        second layer's length = length - 1
        third layer's length = length
        first and second layers need a line feed character at their end: \n.

---
+++ Java/Build a Car.java
@@ -0,0 +1,134 @@
+// https://www.codewars.com/users/patbiz/completed_solutions
+
+public class Car {
+    public Body body;
+    public Chassis chassis;
+    private int lentgh;
+    private int axles;
+    private int doors;
+
+    public Car(int length, int doors) {
+        if(length < 7){
+            throw new IllegalArgumentException("Parameter 'length' is too small");
+        }
+        if(doors < 1 || doors > (length - 3)/2){
+            throw new IllegalArgumentException("Parameter 'door' is either less than 1 or too big");
+        }
+
+        this.lentgh = length;
+        this.doors = doors;
+        this.axles = countAxles();
+
+        this.body = new Body(this.lentgh, this.doors); //at this point two first layers are drawn
+        this.chassis = new Chassis(this.lentgh, this.axles); //third layer is drawn
+    }
+
+    private int countAxles(){
+        if(this.lentgh < 12){
+            return 2;
+        }
+        else return (2 + (this.lentgh - 11 + 1)/2);
+    }
+
+    public String toString(){
+        return this.body.component + this.chassis.component;
+    }
+
+    // definition of other classes
+    public class Body{
+        private String firstLayer; //length - 2
+        private String secondLayer; //length - 1
+        public String component;
+        private int length;
+        private int doors;
+
+        public Body(int length, int doors){
+            this.firstLayer = "";
+            this.secondLayer = "";
+            this.length = length;
+            this.doors = doors;
+
+            drawFirstLayer();
+            drawSecondLayer();
+
+            this.component = firstLayer + secondLayer;
+        }
+
+        private void drawFirstLayer(){
+            this.firstLayer += " ";
+            for(int i = 1; i <= (this.length - 2 - 1); i++){
+                this.firstLayer += "_";
+            }
+            this.firstLayer += "\n";
+        }
+
+        private void drawSecondLayer(){
+            this.secondLayer += "|";
+
+            int howManySpaces = this.length - 3 - this.doors*2;
+            int frontDoors = (this.doors + 1)/2;
+            int rearDoors = this.doors/2;
+
+            for(int i = 1; i <= rearDoors; i++){
+                this.secondLayer += "[]";
+            }
+            for(int i = 1; i <= howManySpaces; i++){
+                this.secondLayer += " ";
+            }
+            for(int i = 1; i <= frontDoors; i++){
+                this.secondLayer += "[]";
+            }
+            this.secondLayer += "\\";
+            this.secondLayer += "\n";
+        }
+
+        public String toString(){
+            return this.component;
+        }
+
+    }
+
+    public class Chassis{
+        public String component; //length
+        private int length;
+        private int axels;
+
+        public Chassis(int length, int axels){
+            this.component = "";
+            this.length = length;
+            this.axels = axels;
+
+            drawThirdLayer();
+        }
+
+        public void drawThirdLayer(){
+            this.component += "-";
+
+            int howManySpaces = this.length - 2 - this.axels*2;
+            int frontAxels = this.axels/2;
+            int rearAxels = this.axels/2;
+
+            if(this.axels == 2){
+                rearAxels = 1;
+            }
+            else if(this.axels%2 == 1){
+                rearAxels++;
+            }
+
+            for(int i = 1; i <= rearAxels; i++){
+                this.component += "o-";
+            }
+            for(int i = 1; i <= howManySpaces; i++){
+                this.component += "-";
+            }
+            for(int i = 1; i <= frontAxels; i++){
+                this.component += "o-";
+            }
+            this.component += "'";
+        }
+
+        public String toString(){
+            return this.component;
+        }
+    }
+}
