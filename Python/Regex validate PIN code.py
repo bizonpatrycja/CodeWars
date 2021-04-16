63c5865bc16d412b4cabbc8b44e0f88a8951d6da
bizonpatrycja <36458654+bizonpatrycja@users.noreply.github.com>
2021-04-16 10:28:43 +0200

    Update Regex validate PIN code.py

--- Python/Regex validate PIN code.py
+++ Python/Regex validate PIN code.py
@@ -1,3 +1,5 @@
+# https://www.codewars.com/kata/55f8a9c06c018a0d6e000132
+
 def validate_pin(pin):
     if (len(pin) == 4 or len(pin) == 6) and all([s.isdigit() == True for s in pin]):
         return True
